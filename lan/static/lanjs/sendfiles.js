3c08d3c079cbcb146bb8ab6c0cedc6e4791cf54b
Sandarva3 <Sandarva_email>
2024-07-15 20:16:06 +0545

    FILE UPLOADING MECHANISM IS WAAYYY TOOO FASTERRR NOWWWW

--- lan/static/lanjs/sendfiles.js
+++ lan/static/lanjs/sendfiles.js
@@ -6,7 +6,7 @@ uploadBtn.addEventListener('click', () =>{
     uploadBtn.style.display = 'none';
     waitArea.style.display = 'block';
     getIP(filesInArea);
-})
+});
 });
 
 function getIP(filesInArea){
@@ -20,7 +20,37 @@ function getIP(filesInArea){
     });
 };
 
-function sendFiles(filesInArea, ipAddress){
+
+async function sendFiles(filesInArea, ipAddress){
+    const _filesPromises = [];
+    for(let i=0; i<filesInArea.length; i++){
+        let formData = new FormData();
+        formData.append('file', filesInArea[i]);
+        formData.append('ipAddress', ipAddress);
+
+        const _filePromise = fetch(fileURL, {
+            method: "POST",
+            headers:{
+                'X-CSRFToken': csrfToken3
+            },
+            body: formData
+        });
+        _filesPromises.push(_filePromise);
+    }
+
+    try {
+        const responses = await Promise.all(_filesPromises);
+        console.log("HOGAYA BHAI..");
+        location.reload();
+    } catch (error) {
+        console.error("NAHI HUA BHAI...", error);
+    }
+};
+
+
+
+
+/*  function sendFiles(filesInArea, ipAddress){
     let formData = new FormData();
     for(let i=0; i<filesInArea.length; i++){
         formData.append('file', filesInArea[i]);
@@ -41,4 +71,4 @@ function sendFiles(filesInArea, ipAddress){
     .catch(error => {
         console.error('Error:', error);
     });
-};
+};  */
