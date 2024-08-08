bf115b431894af462139fb7a821be93a2171ed9c
Sandarva3 <Sandarva_email>
2024-08-08 08:33:28 +0545

    tailwind Installation on local machine

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './lan/templates/**/*.html',
+    './lan/static/lancss/*.css',
+    './lan/static/lanjs/scripts.js',
+    './lan/static/lanjs/locale.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
